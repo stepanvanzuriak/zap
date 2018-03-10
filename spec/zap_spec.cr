cb3e0656630035a0b4b7bca5e784d51c3a2546c0
stepanvanzuriak <stepanvanzuriak@gmail.com>
2018-03-10 17:41:51 +0200

    Add compact and concat

--- spec/zap_spec.cr
+++ spec/zap_spec.cr
@@ -3,5 +3,17 @@ require "./spec_helper"
 describe Zap do
   it "@chunk Creates an array of elements split into groups the length of size" do
     Zap.chunk(['a', 'b', 'c', 4], 2).should eq([['a', 'b'], ['c', 4]])
+    Zap.chunk(['a', 'b', 'c', 'd'], 3).should eq([['a', 'b', 'c'], ['d']])
+  end
+
+  it "@compact Creates an array with all falsey values removed" do
+    Zap.compact(["a", nil, "b", nil, "c", nil]).should eq(["a", "b", "c"])
+    Zap.compact([0, 1, false, 2, "", 3]).should eq([1, 2, 3])
+  end
+
+  it "@concat Creates a new array concatenating array with any additional arrays and/or values" do
+    Zap.concat([1], 2, [3], [[4]]).should eq([1, 2, 3, [4]])
+    Zap.concat([1, 2], 2, [3], [[4]], [[[4]]]).should eq([1, 2, 2, 3, [4], [[4]]])
+    Zap.concat([1, 2], 2, [3, 10], [[4]], [[[4]]]).should eq([1, 2, 2, 3, 10, [4], [[4]]])
   end
 end
--- src/zap.cr
+++ src/zap.cr
@@ -12,4 +12,27 @@ module Zap
 
     result
   end
+
+  def compact(array)
+    array = array.select { |element| element }
+    array = array.select { |element| element != 0 && element != "" }
+
+    array
+  end
+
+  def concat(array, *values)
+    values_array = values.to_a
+
+    values_array.each_with_index do |value, index|
+      if value.is_a?(Array)
+        values_array.delete_at(index)
+
+        value.reverse.each do |element|
+          values_array.insert(index, element)
+        end
+      end
+    end
+
+    array + values_array
+  end
 end
